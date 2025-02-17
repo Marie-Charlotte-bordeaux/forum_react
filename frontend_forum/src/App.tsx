c253364033940615b389011846e6db7b0e21a190
mbordeaux <mbordeaux@amx1.io>
2025-02-17 11:25:56 +0100

    first commit front

---
+++ frontend_forum/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import './index.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <main>
+    <h1 className="text-3xl font-bold underline">
+      Hello  Forum!
+    </h1>
+
+    </main>
+    </>
+  )
+}
+
+export default App
