e1c839c4730b41b32b4211ae0ef98fa30d45ad89
LaigleJ <you@example.com>
2025-02-17 11:36:42 +0100

    add .env and .gitignore

--- back_forum/src/index.ts
+++ back_forum/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("Server running on http://localhost:5000");
 });
